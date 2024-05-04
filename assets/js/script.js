46071d453ecf61bac450dcb620f408af8860d324
ANK <kalupahanaadheetha5@gmail.com>
2024-05-04 14:29:54 +0530

    body sale back button and title style complete

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+$("#btn-login").on('click',() =>{
+    $("#body-menu").css("left",0);
+    $("#body-login").css("z-index",0);
+})
+$("#sidebar-menu > .sale").on('click',() =>{
+    alert("sale :)")
+})
+$("#sidebar-menu > .supplier").on('click',() =>{
+    alert("supplier :)")
+})
+$("#sidebar-menu > .customer").on('click',() =>{
+    alert("customer :)")
+})
+$("#sidebar-menu > .employee").on('click',() =>{
+    alert("employee :)")
+})
+$("#sidebar-menu > .inventory").on('click',() =>{
+    alert("inventory :)")
+})
+$("#sidebar-menu > .admin").on('click',() =>{
+    alert("admin :)")
+})
+$("#sidebar-menu > .user-service").on('click',() =>{
+    alert("user service :)")
+})
