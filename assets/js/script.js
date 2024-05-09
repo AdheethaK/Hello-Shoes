2bbb45633f853fdc9c729bd8c146d565203e856d
ANK <kalupahanaadheetha5@gmail.com>
2024-05-09 16:05:50 +0530

    body customer gender option input complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,67 @@
 $("#btn-login").on('click',() =>{
-    $("#body-menu").css("left",0);
+    $("#body-menu").css("left",0).css("z-index",1);
     $("#body-login").css("z-index",0);
 })
+// sale
 $("#sidebar-menu > .sale").on('click',() =>{
-    alert("sale :)")
+    $("#body-sale").css("left",0).css("z-index",1);
+    $("#body-menu").css("z-index",0);
 })
+$("#body-sale > div > .btn-back-to-menu").on('click',()=>{
+    $("#body-sale").css("left","-100%").css("z-index",0);
+    $("#body-menu").css("z-index",1);
+})
+// supplier
 $("#sidebar-menu > .supplier").on('click',() =>{
-    alert("supplier :)")
+    $("#body-supplier").css("left",0).css("z-index",1);
+    $("#body-menu").css("z-index",0);
 })
+$("#body-supplier > div > .btn-back-to-menu").on('click',()=>{
+    $("#body-supplier").css("left","-100%").css("z-index",0);
+    $("#body-menu").css("z-index",1);
+})
+// customer
 $("#sidebar-menu > .customer").on('click',() =>{
-    alert("customer :)")
+    $("#body-customer").css("left",0).css("z-index",1);
+    $("#body-menu").css("z-index",0);
 })
+$("#body-customer > div > .btn-back-to-menu").on('click',()=>{
+    $("#body-customer").css("left","-100%").css("z-index",0);
+    $("#body-menu").css("z-index",1);
+})
+// employee
 $("#sidebar-menu > .employee").on('click',() =>{
-    alert("employee :)")
+    $("#body-employee").css("left",0).css("z-index",1);
+    $("#body-menu").css("z-index",0);
 })
+$("#body-employee > div > .btn-back-to-menu").on('click',()=>{
+    $("#body-employee").css("left","-100%").css("z-index",0);
+    $("#body-menu").css("z-index",1);
+})
+// inventory
 $("#sidebar-menu > .inventory").on('click',() =>{
-    alert("inventory :)")
+    $("#body-inventory").css("left",0).css("z-index",1);
+    $("#body-menu").css("z-index",0);
 })
+$("#body-inventory > div > .btn-back-to-menu").on('click',()=>{
+    $("#body-inventory").css("left","-100%").css("z-index",0);
+    $("#body-menu").css("z-index",1);
+})
+// admin
 $("#sidebar-menu > .admin").on('click',() =>{
-    alert("admin :)")
+    $("#body-admin").css("left",0).css("z-index",1);
+    $("#body-menu").css("z-index",0);
 })
+$("#body-admin > div > .btn-back-to-menu").on('click',()=>{
+    $("#body-admin").css("left","-100%").css("z-index",0);
+    $("#body-menu").css("z-index",1);
+})
+// user services
 $("#sidebar-menu > .user-service").on('click',() =>{
-    alert("user service :)")
+    $("#body-user-service").css("left",0).css("z-index",1);
+    $("#body-menu").css("z-index",0);
+})
+$("#body-user-service > div > .btn-back-to-menu").on('click',()=>{
+    $("#body-user-service").css("left","-100%").css("z-index",0);
+    $("#body-menu").css("z-index",1);
 })
